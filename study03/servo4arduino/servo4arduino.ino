883dc6c65453cf13259e3bcc8fe1659a3a2d96a5
freshmea <freshmea@naver.com>
2022-10-04 16:47:10 +0900

    arduino led + servo + web JS

--- study03/servo4arduino/servo4arduino.ino
+++ study03/servo4arduino/servo4arduino.ino
@@ -1,3 +1,6 @@
+#include <Servo.h> 
+int servoPin = 10;
+int angle = 0;
 const unsigned int rledPin = 11;
 const unsigned int gledPin = 12;
 const unsigned int bledPin = 13;
@@ -5,8 +8,10 @@ const unsigned int potPin = A0;
 
 String  inputString    = "";
 boolean stringComplete = false;
-
+Servo servo;
 void setup() {
+
+  servo.attach(servoPin); 
   pinMode(rledPin, OUTPUT);
   pinMode(gledPin, OUTPUT);
   pinMode(bledPin, OUTPUT);
@@ -16,8 +21,9 @@ void setup() {
 
 void loop() {
   unsigned int adcVal = analogRead(potPin);
-  Serial.print("adc");  Serial.println(adcVal);
-  
+  Serial.print("adc");  
+  Serial.println(adcVal);
+//  servo.write(adcVal/7);
   if(stringComplete) 
   {
     if(inputString.substring(0,3) == "led")
@@ -34,6 +40,21 @@ void loop() {
         digitalWrite(bledPin, HIGH );
       else if(inputString.substring(3,4) == "5")
         digitalWrite(bledPin, LOW );
+    }
+    if(inputString.substring(0,3) == "sev")
+    {
+      if (inputString.substring(3,4) == "0"){
+        servo.write(45);
+        delay(15);
+      }
+      else if(inputString.substring(3,4) == "1"){
+        servo.write(90);
+        delay(15);
+      }
+      else if(inputString.substring(3,4) == "2"){
+        servo.write(135);
+        delay(15);
+      }
     }
     Serial.println(inputString);
     inputString = "";
